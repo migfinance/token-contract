8ee847ec551692f628c7dd61e7286edeb1fa9ec2
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2021-06-27 03:06:52 +0530

    Added staking contract

--- contracts/staking/Staking.sol
+++ contracts/staking/Staking.sol
@@ -9,140 +9,123 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Staking is Context, Ownable, ReentrancyGuard {
     address public stakeToken;
     address public rewardToken;
-    // uint256 public SECONDS_IN_A_DAY = 28800;
-    uint256 public constant SECONDS_IN_A_DAY = 1;
-    uint256 public constant STAKING_DAYS_MIN = 7;
-    uint256 public constant STAKING_DAYS_REWARD = 30;
-    uint256 public constant STAKING_AMOUNT_MIN = 10000000000000000000; //10
-    uint256 public constant VESTING_PERIOD = 60;
-    uint256 public constant VESTING_AMOUNT = 100000000000000000000000; //100000
-    uint256 public constant REWARD_PER_DAY = 5000000000000000000000; //5000
+    uint256 public totalStakedAmount;
 
-    uint256 vestingStart;
-    uint256 totalStakingAmount;
+    //2 years + 1 leap year => 24 * 60 * 60 * (3 * 365 + 1)
+    uint256 public constant STAKING_PERIOD = 94694400;
 
     struct DepositInfo {
         uint256 amount;
-        uint256 time;
-        bool isLpToken;
+        uint256 startBlock;
+        uint256 amountWithdrawn;
     }
 
     mapping(address => DepositInfo[]) public userInfo;
-    event Staked(address indexed sender, uint256 amount, bool isLpToken);
+    event Staked(address indexed sender, uint256 amount, uint256 stakeId);
 
-    constructor(address _stakeToken, address _rewardToken) {
-        stakeToken = _stakeToken;
-        rewardToken = _rewardToken;
-        vestingStart = block.number;
+    modifier validateID(uint256 id) {
+        require(
+            id < userInfo[msg.sender].length,
+            "Staking:checkReward:: ERR_INVALID_ID"
+        );
     }
 
-    function staking(uint256 amount, bool isLpToken) external {
+    constructor(address _stakeToken, address _rewardToken) {
         require(
-            amount >= STAKING_AMOUNT_MIN,
-            "Staking: Invalid Staking Amount"
+            _stakeToken != address(0),
+            "Staking:constructor:: ERR_ZERO_ADDRESS_STAKE_TOKEN"
         );
-        if (isLpToken) {
-            IERC20(rewardToken).transferFrom(msg.sender, address(this), amount);
-        } else {
-            IERC20(stakeToken).transferFrom(msg.sender, address(this), amount);
-        }
+        require(
+            _rewardToken != address(0),
+            "Staking:constructor:: ERR_ZERO_ADDRESS_REWARD_TOKEN"
+        );
+
+        stakeToken = _stakeToken;
+        rewardToken = _rewardToken;
+    }
+
+    function stake(uint256 amount) external {
+        require(amount != 0, "Staking:stake:: ERR_STAKE_AMOUNT");
 
         DepositInfo memory depositInfo;
         depositInfo.amount = amount;
-        depositInfo.time = block.number;
-        depositInfo.isLpToken = isLpToken;
+        depositInfo.startBlock = block.number;
         userInfo[msg.sender].push(depositInfo);
 
-        totalStakingAmount = totalStakingAmount + (amount);
+        uint256 stakeId = userInfo[msg.sender].length - 1;
+        totalStakedAmount = totalStakedAmount + amount;
 
-        emit Staked(msg.sender, amount, isLpToken);
+        IERC20(stakeToken).transferFrom(msg.sender, address(this), amount);
+        emit Staked(msg.sender, amount, stakeId);
     }
 
-    function checkReward(uint256 id) public view returns (uint256) {
-        require(id < userInfo[msg.sender].length, "Staking: Invalid Id");
+    function checkReward(uint256 id) public view returns (uint256 reward) {
+        if (id < userInfo[msg.sender].length) return 0;
+
         DepositInfo storage depositInfo = userInfo[msg.sender][id];
-        require(
-            depositInfo.time + STAKING_DAYS_MIN * SECONDS_IN_A_DAY <=
-                block.number,
-            "Staking: Can not unstaking yet"
-        );
 
-        uint256 reward;
-        uint256 rewardFactor = 1;
-        if (
-            depositInfo.time + STAKING_DAYS_REWARD * SECONDS_IN_A_DAY <=
-            block.number
-        ) {
-            rewardFactor = 2; //should be updated
-        }
+        //reward = (currentTime/totalTime) * totalAmountStaked - amountWithdrawn
+        uint256 canClaim = depositInfo.amount *
+            ((block.number - depositInfo.startBlock) / STAKING_PERIOD);
 
-        if (depositInfo.isLpToken) {
-            reward =
-                ((depositInfo.amount *
-                    (block.number - depositInfo.time) *
-                    REWARD_PER_DAY *
-                    75) / (100 * totalStakingAmount)) *
-                rewardFactor;
-        } else {
-            reward =
-                ((depositInfo.amount *
-                    (block.number - depositInfo.time) *
-                    REWARD_PER_DAY *
-                    25) / (100 * totalStakingAmount)) *
-                rewardFactor;
-        }
-
-        return reward;
+        reward = canClaim - depositInfo.amountWithdrawn;
     }
 
-    function depositCount() public view returns (uint256) {
+    function depositCount() external view returns (uint256) {
         return userInfo[msg.sender].length;
     }
 
-    function unstaking(uint256 id, bool isLpToken) external {
-        uint256 reward = checkReward(id);
-        if (isLpToken) {
-            IERC20(rewardToken).transfer(
-                msg.sender,
-                userInfo[msg.sender][id].amount + reward
-            );
-        } else {
-            IERC20(stakeToken).transfer(
-                msg.sender,
-                userInfo[msg.sender][id].amount + reward
-            );
-        }
-        totalStakingAmount =
-            totalStakingAmount -
-            (userInfo[msg.sender][id].amount);
-        userInfo[msg.sender][id] = userInfo[msg.sender][
-            userInfo[msg.sender].length - 1
-        ];
-        delete userInfo[msg.sender][userInfo[msg.sender].length - 1];
-        userInfo[msg.sender].pop();
+    function claim(uint256 id) external validateID(id) {
+        uint256 toClaim = checkReward(id);
+        if (toClaim == 0) return;
+
+        toClaim = _update(id, toClaim);
+        totalStakedAmount = totalStakedAmount - toClaim;
+
+        IERC20(rewardToken).transfer(msg.sender, toClaim);
+        IERC20(stakeToken).transfer(msg.sender, toClaim);
     }
 
-    function withdrawToken(uint256 amount, bool isLpToken) external onlyOwner {
-        if (isLpToken) {
-            IERC20(rewardToken).transfer(msg.sender, amount);
-        } else {
-            IERC20(stakeToken).transfer(msg.sender, amount);
+    function _update(uint256 id, uint256 toClaim)
+        internal
+        returns (uint256 claimAmount)
+    {
+        DepositInfo storage depositInfo = userInfo[msg.sender][id];
+
+        claimAmount = toClaim;
+        if (depositInfo.amount < depositInfo.amountWithdrawn + toClaim) {
+            claimAmount = depositInfo.amount - depositInfo.amountWithdrawn;
+        }
+
+        depositInfo.amountWithdrawn += claimAmount;
+
+        //remove user id if all tokens claimed
+        if (depositInfo.amountWithdrawn == depositInfo.amount) {
+            depositInfo = userInfo[msg.sender][userInfo[msg.sender].length - 1];
+            delete userInfo[msg.sender][userInfo[msg.sender].length - 1];
+            userInfo[msg.sender].pop();
         }
     }
 
-    function setTBIT(address _stakeToken) external onlyOwner {
+    //In case tokens get stuck inside contract
+    function withdrawToken(uint256 amount, address token) external onlyOwner {
+        if (IERC20(token).balanceOf(address(this)) >= amount)
+            IERC20(token).transfer(msg.sender, amount);
+    }
+
+    function setStakeToken(address _stakeToken) external onlyOwner {
+        require(
+            _stakeToken != address(0),
+            "Staking:constructor:: ERR_ZERO_ADDRESS_STAKE_TOKEN"
+        );
         stakeToken = _stakeToken;
     }
 
-    function setLPToken(address _rewardToken) external onlyOwner {
-        rewardToken = _rewardToken;
-    }
-
-    function withdrawLiquidity(address to) external onlyOwner {
+    function setRewardToken(address _rewardToken) external onlyOwner {
         require(
-            vestingStart + VESTING_PERIOD * SECONDS_IN_A_DAY <= block.number,
-            "Staking: Vesting is not available yet"
+            _rewardToken != address(0),
+            "Staking:constructor:: ERR_ZERO_ADDRESS_REWARD_TOKEN"
         );
-        IERC20(stakeToken).transfer(to, VESTING_AMOUNT);
+        rewardToken = _rewardToken;
     }
 }
